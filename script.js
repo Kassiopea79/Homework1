041795de386a50f5bf80547734e974f329fd73a0
Irina Burnaby <kassiopeamaratik@gmail.com>
2023-02-10 18:31:41 -0500

    Edit script

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
 // task 1
 
- function whichSeason(month) {
-    const season = prompt("Введите число:");
-    if (month === 1 && month === 2 || month === 12) {
-        return "Зима";
-    } else if (month >= 3 && month <= 5) {
-        return "Весна";
-    } else if (month >= 6 && month <= 8) {
-        return "Лето";
-    } if (month >= 9 && month <= 11) {
-        return "Осень";
-    } else {
-        return "Вы ввели неверное значение!";
-    }
+function whichSeason() {
+  let month = prompt("Введите число 1 - 12: ");
+  if ((month === 1 || month === 2) || month === 12) {
+    return "Зима";
+  } else if (month >= 3 && month <= 5) {
+    return "Весна";
+  } else if (month >= 6 && month <= 8) {
+    return "Лето";
+  }
+  if (month >= 9 && month <= 11) {
+    return "Осень";
+  } else {
+    return "Вы ввели неверное значение!";
+  }
 }
-console.log(whichSeason(5));
+let season = whichSeason();
+console.log("Сейчас сезон: " + season);
 
-       // Homework 6
+// Homework 6
 
 // task 1
 
 let arr = [1, 5, 4, 10, 0, 3];
 for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
-    if (arr[i] === 10) {
-        break;
-    }
+  console.log(arr[i]);
+  if (arr[i] === 10) {
+    break;
+  }
 }
 
 // task 2
@@ -37,7 +39,7 @@ console.log(elementPosition);
 // task 3
 
 let letJoin = [1, 3, 5, 10, 20];
-letJoin = letJoin.join(' ');
+letJoin = letJoin.join(" ");
 console.log(letJoin);
 
 // task 4
@@ -50,7 +52,7 @@ for (let i = 0; i < 10; i++) {
   firstArray.push(randomNumber);
   if (randomNumber % 2 === 0) {
     secondArray.push(randomNumber);
-  } 
+  }
 }
 console.log(firstArray);
 console.log(secondArray);
@@ -76,13 +78,13 @@ console.log(addNumber);
 
 // task 7
 
-let sortArray = [9, 8, 7, 'a', 6, 5];
+let sortArray = [9, 8, 7, "a", 6, 5];
 
-sortArray = sortArray.filter(function(item) {
-  return typeof item === 'number';
+sortArray = sortArray.filter(function (item) {
+  return typeof item === "number";
 });
 
-sortArray.sort(function(a, b) {
+sortArray.sort(function (a, b) {
   return a - b;
 });
 
@@ -94,17 +96,17 @@ let ifNumber = [9, 8, 7, 6, 5];
 
 let guessNumber = Number(prompt("Введите любую цифру от 1 до 10: "));
 if (ifNumber.includes(guessNumber)) {
-    console.log(`Число ${guessNumber} находится в массиве!`)
+  console.log(`Число ${guessNumber} находится в массиве!`);
 } else {
-    console.log(`Число ${guessNumber} отсутствует в массиве.`)
+  console.log(`Число ${guessNumber} отсутствует в массиве.`);
 }
 
 // task 9
 
-let str = 'abcdef';
-let reverseStr = str.split('');
+let str = "abcdef";
+let reverseStr = str.split("");
 reverseStr.reverse();
-reverseStr = reverseStr.join('');
+reverseStr = reverseStr.join("");
 console.log(reverseStr);
 
 // task 10
@@ -113,17 +115,20 @@ let randomArr = [];
 let sum = 0;
 
 for (let i = 0; i < 6; i++) {
-    const randomNum = Math.floor(Math.random() * 10);
-    randomArr.push(randomNum);
-    sum += randomNum;
+  const randomNum = Math.floor(Math.random() * 10);
+  randomArr.push(randomNum);
+  sum += randomNum;
 }
 console.log(randomArr);
-const avgNum =Math.floor(sum / randomArr.length);
+const avgNum = Math.floor(sum / randomArr.length);
 console.log(avgNum);
 
 // task 11
 
-let arrConcat = [[1, 2, 3],[4, 5, 6]];
+let arrConcat = [
+  [1, 2, 3],
+  [4, 5, 6],
+];
 let newConcat = [].concat(...arrConcat);
 console.log(newConcat);
 
