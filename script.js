c408692855f007b4f26a3184895fd44f00ee1a3a
Irina Burnaby <kassiopeamaratik@gmail.com>
2023-02-12 11:42:24 -0500

    Edit script

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 // task 1
 
 function whichSeason() {
-  let month = prompt("Введите число 1 - 12: ");
+  let month = Number(prompt("Введите число 1 - 12: "));
   if (month === 1 || month === 2 || month === 12) {
-    return "Зима";
+    console.log("Зима");
+    alert("Зима");
   } else if (month >= 3 && month <= 5) {
-    return "Весна";
+    console.log("Весна");
+    alert("Весна");
   } else if (month >= 6 && month <= 8) {
-    return "Лето";
-  }
-  if (month >= 9 && month <= 11) {
-    return "Осень";
+    console.log("Лето");
+    alert("Лето");
+  } else if (month >= 9 && month <= 11) {
+    console.log("Осень");
+    alert("Осень");
   } else {
-    return "Вы ввели неверное значение!";
+    console.log("Вы ввели неверное значение!");
+    alert("Вы ввели неверное значение!");
   }
+  return;
 }
-let season = whichSeason();
-console.log("Сейчас сезон: " + season);
+
+
 
 
 
